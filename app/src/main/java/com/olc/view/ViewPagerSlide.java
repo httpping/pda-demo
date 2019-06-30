8c74559c4e8566b59b3e10e982b12bf218e6b779
tanping <871625458@qq.com>
2019-06-30 12:00:25 +0800

    第一次提交xxxxxx
    Signed-off-by: tanping <871625458@qq.com>

--- app/src/main/java/com/olc/view/ViewPagerSlide.java
+++ app/src/main/java/com/olc/view/ViewPagerSlide.java
@@ -1,38 +1,1 @@
-package com.olc.view;
-
-import android.content.Context;
-import android.support.v4.view.ViewPager;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-
-public class ViewPagerSlide extends ViewPager {
-    //Is it possible to slide?
-    private boolean isSlide = true;
-
-    public void setSlide(boolean slide) {
-        isSlide = slide;
-    }
-
-    public ViewPagerSlide(Context context) {
-        super(context);
-    }
-
-    public ViewPagerSlide(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return isSlide && super.onInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        return isSlide && super.onTouchEvent(ev);
-    }
-
-    @Override
-    public void setOffscreenPageLimit(int limit) {
-        super.setOffscreenPageLimit(limit);
-    }
-}
+package com.olc.view;import android.content.Context;import android.support.v4.view.ViewPager;import android.util.AttributeSet;import android.view.MotionEvent;public class ViewPagerSlide extends ViewPager {    //Is it possible to slide?    private boolean isSlide = true;    public void setSlide(boolean slide) {        isSlide = slide;    }    public ViewPagerSlide(Context context) {        super(context);    }    public ViewPagerSlide(Context context, AttributeSet attrs) {        super(context, attrs);    }    @Override    public boolean onInterceptTouchEvent(MotionEvent ev) {        return isSlide && super.onInterceptTouchEvent(ev);    }    @Override    public boolean onTouchEvent(MotionEvent ev) {        return isSlide && super.onTouchEvent(ev);    }    @Override    public void setOffscreenPageLimit(int limit) {        super.setOffscreenPageLimit(limit);    }}
